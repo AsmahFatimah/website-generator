52b50b64d4f6be9348b8818a6dcb275171d0c6e8
Asmah Fatimah <asmahfatima7@gmail.com>
2026-03-25 21:24:17 +0500

    Delete

--- backend/middleware/Logger.js
+++
@@ -1,7 +0,0 @@
-const randomId = Math.random()
-const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}${randomId}`);
-    next()
-}
-
-module.exports = logger
